8e00c6218c64752877c6a1cfaab3113241a32ea3
Evgeniy Krokun <treschelet@fry.(none)>
2014-07-12 18:36:11 +0400

    news model

---
+++ models/News.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace treschelet\news\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $image
+ * @property string $summary
+ * @property string $content
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['summary', 'content'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['title', 'image'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'image' => 'Image',
+            'summary' => 'Summary',
+            'content' => 'Content',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
