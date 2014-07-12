f2f68d2fa037e4eab0c0853b50a0c2184b606110
Evgeniy Krokun <treschelet@fry.(none)>
2014-07-12 12:20:49 +0400

    migrations

---
+++ migrations/m000000_000001_CreateNewsTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Schema;
+
+
+class m000000_000001_CreateNewsTable extends \yii\db\Migration
+{
+    public function safeUp()
+    {
+        switch (Yii::$app->db->driverName) {
+            case 'mysql':
+                $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+                break;
+            case 'pgsql':
+                $tableOptions = null;
+                break;
+            default:
+                throw new RuntimeException('Your database is not supported!');
+        }
+
+        $this->createTable('{{%news}}', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING . '(255) NOT NULL',
+            'image' => Schema::TYPE_STRING . '(255)',
+            'summary' => Schema::TYPE_TEXT,
+            'content'  => schema::TYPE_TEXT,
+            'created_at' => Schema::TYPE_TIMESTAMP . ' DEFAULT CURRENT_TIMESTAMP',
+            'updated_at' => Schema::TYPE_DATETIME,
+        ], $tableOptions);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%news}}');
+    }
+} 
